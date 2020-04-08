21a4a4c774595049f407fcfcc199826b12ec75ce
biggoober <biggoober@kali>
2020-04-08 14:47:34 -0500

    Initial file commit

---
+++ book_creater.py
@@ -0,0 +1,4 @@
+#This file will have both books and chapters as objects.
+#Books will be collections of chapters. This file should also be able
+#to return a newly created book, and thus create books, to the main library_controller to allow for this book to be written out to the json file.
+
---
+++ library_controller.py
@@ -0,0 +1,4 @@
+#This file will deal with all the JSON parsing, both in and out, to allow for
+#book manipulation in this library.
+#This file should also be the main command line application, allowing for pretty-printing of 
+#the entire library. This should also allow us to randomly choose a page from ALL BOOKS, a subgenre of books.
