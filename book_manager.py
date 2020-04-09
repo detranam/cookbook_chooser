a9d7c09b5d7c4ff953c5914afa560b61dc1cac09
biggoober <biggoober@kali>
2020-04-09 09:53:37 -0500

    json file change

--- book_manager.py
+++ book_manager.py
@@ -6,29 +6,28 @@
 import json
 class Book(object):
     def __init__(self):
-        _book_string = "{"
+        self._book_string = "{"
         title = input("Book title:")
         book_type = input("Type- [specialty, drink, food] :")
         num_chapters = (int)(input("number of chapters:"))
         length = input("Book length:")
         description = input("Book description:")
         tempvar = 0
-        _book_string = _book_string + ("\"title\":\"{title}\"," +
-                  "\"type\":\"{book_type}\","+
-                "\"length\":\"{length}\","+
-                "\"description\":\"{description}\"}")#todo replace this } with ,
-        #todo remove this useless test, uncomment the rest
-        with open("bigtest.json", "w") as writefile:
-            json.dump(json.loads(_book_string), writefile)
+        _book_string = _book_string + (f"\"title\":\"{title}\"," +
+            f"\n\"type\":\"{book_type}\","+
+            f"\n\"length\":\"{length}\","+
+            f"\n\"description\":\"{description}\","+
+            f"\"chapters\":[")
 
-        #+
-         #       "\"chapters\":["
-
-
-       # while var < num_chapters:
-       #     add_chapter()
-    def add_chapter():
-        chap_name = input("Enter chapter name: ")
-        start_page = (int)(input("Enter start page for {}: ", chap_name))
-        end_page = (int)(input("Enter ending page for {}: ", chap_name))
-        _book_string = _book_string + "{"#todo finish this
+    def add_chapter(num_chapters):
+        for x in range(num_chapters-1):
+            chap_name = input("Enter chapter name: ")
+            start_page = (int)(input("Enter start page for {}: ", chap_name))
+            end_page = (int)(input("Enter ending page for {}: ", chap_name))
+            _book_string = _book_string + ("{" +
+                f"\n\"title\":\"{chap_name}\","+
+                f"\n\"start_page\":\"{starting_page}\","+
+                f"\n\"end_page\":\"{end_page}\""+
+                "}")
+        _book_string = _book_string + "]\n}"
+        return _book_string
