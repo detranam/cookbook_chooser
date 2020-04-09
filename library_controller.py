009af089a410dcef593755fd356387481a346ba3
biggoober <biggoober@kali>
2020-04-08 20:25:38 -0500

    removed files except again

--- library_controller.py
+++
@@ -1,8 +0,0 @@
-#This file will deal with all the JSON parsing, both in and out, to allow for
-#book manipulation in this library.
-#This file should also be the main command line application, allowing for pretty-printing of 
-#the entire library. This should also allow us to randomly choose a page from ALL BOOKS, a subgenre of books.
-
-
-import json
-from tabulate import tabulate
