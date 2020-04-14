7a6ec3cec0902b90fb33b1455d271d0eaa7492ed
Matthew DeTrana <matthew.detra@gmail.com>
2020-04-14 00:03:01 -0500

    random recipe implemented

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 ##Goals
 [x] Be able to parse a json list of cookbooks
 [x] Be able to accept a new book and append it to the json library
-[ ] Be able to randomly recieve a recipe from a single book
-[ ] Be able to randomly recieve a recipe from a single type (food, drink, etc)
-[ ] Be able to list all kinds of books (pretty printed)
+[ ] Be able to randomly recieve a recipe (by page) from a selected book
+[x] Be able to randomly recieve a recipe from any book
 [x] Be able to list all books (pretty printed)
--- library.py
+++ library.py
@@ -10,6 +10,7 @@ import argparse
 import book_manager
 import os
 import sys
+import random
 from tabulate import tabulate
 parser = argparse.ArgumentParser()
 parser.add_argument("-a", "--addbook", action="count", default=0,
@@ -21,8 +22,8 @@ parser.add_argument("-r", "--randomrecipe", action="count", default=0,
 args = parser.parse_args()
 
 if not len(sys.argv) > 1:
-    print("You must specify an argument!")
-    print("Try \'library.py --help\' for more information")
+    print("\nERROR: NoArgumentPassed")
+    print("Try \'library.py --help\' for more information\n")
     exit(1)
 
 
@@ -42,6 +43,7 @@ if args.addbook:
     if(os.path.exists('library.json')):
         add_book()
     else:
+        print("Autocreating library: Please add books")
         with open("library.json", "w") as initial_write:
             initial_write.write("{\"library\": []}")
         add_book()
@@ -50,14 +52,41 @@ if args.listbooks:
     if(os.path.exists('library.json')):
         with open("library.json", "r") as read_file:
             booklist = json.load(read_file)
-            headers = ["Book Index", "Title"]
+            headers = ["Book Index", "Title", "Genre"]
             book_printer = []
             x = 0
             while x < len(booklist["library"]):
-                book_to_add = [x, booklist["library"][x]["title"]]
+                book_to_add = [x, booklist["library"][x]["title"],
+                    booklist["library"][x]["type"]]
                 book_printer.append(book_to_add)
                 x += 1
 
-            print(tabulate(book_printer, headers, tablefmt="fancy_grid"))
+            print(tabulate(book_printer, headers, tablefmt="grid"))
     else:
         print("No library.json file exists")
+
+if args.randomrecipe:
+    if(os.path.exists('library.json')):
+        with open("library.json", "r") as read_file:
+            book_list = json.load(read_file)
+            # get the amount of books
+            book_count = len(book_list["library"])
+            book_selection = random.randint(0, book_count-1)
+            book_name = book_list["library"][book_selection]["title"]
+            # get the amount of chapters
+            chapter_count = len(book_list["library"][book_selection]["chapters"])
+            chapter_selection = random.randint(0, chapter_count-1)
+            chapter_name = book_list["library"][book_selection]["chapters"][chapter_selection]["title"]
+            # choose a page within the page numbers
+            start_page = (int)(book_list["library"][book_selection]["chapters"][chapter_selection]["start_page"])
+            end_page = (int)(book_list["library"][book_selection]["chapters"][chapter_selection]["end_page"])
+            page_selection = random.randint(start_page, end_page)
+            # just do a format single string then print it
+            recipe_string = (f"Make a recipe from \'{book_name}\'. It is" +
+                f" in chapter \'{chapter_name}\', on page {page_selection}")
+            print(recipe_string)
+    else:
+        print("Autocreating library: Please add books")
+        with open("library.json", "w") as initial_write:
+            initial_write.write("{\"library\": []}")
+        add_book()
